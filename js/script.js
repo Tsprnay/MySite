e3842d74ddc8bb66a576f836d459eb01f265686a
Tsprnay <65571116+Tsprnay@users.noreply.github.com>
2023-04-30 02:43:46 +0200

    My new site

---
+++ js/script.js
@@ -0,0 +1,81 @@
+function getRandomPosition(rectangle, size) {
+  const edge = Math.floor(Math.random() * 4);
+  const max_x = rectangle.clientWidth - size;
+  const max_y = rectangle.clientHeight - size;
+  const offset = size * 2;
+
+  switch (edge) {
+    case 0:
+      return {
+        x: Math.floor(Math.random() * max_x),
+        y: -offset,
+      };
+    case 1:
+      return {
+        x: rectangle.clientWidth + offset,
+        y: Math.floor(Math.random() * max_y),
+      };
+    case 2:
+      return {
+        x: Math.floor(Math.random() * max_x),
+        y: rectangle.clientHeight + offset,
+      };
+    case 3:
+    default:
+      return {
+        x: -offset,
+        y: Math.floor(Math.random() * max_y),
+      };
+  }
+}
+
+
+
+function createRandomCircle() {
+  const rectangle = document.querySelector(".rectangle");
+  const circle = document.createElement("div");
+  const size = Math.floor(Math.random() * 100) + 10;
+  const position = getRandomPosition(rectangle, size);
+  const duration = 5 + Math.random() * 5;
+  const finalPosition = getRandomPosition(rectangle, size);
+
+  circle.style.width = `${size}px`;
+  circle.style.height = `${size}px`;
+  circle.style.left = `${position.x}px`;
+  circle.style.top = `${position.y}px`;
+  circle.style.backgroundColor = "rgba(255, 0, 255, 1)";
+  circle.style.position = "absolute";
+  circle.style.borderRadius = "50%";
+  circle.style.filter = "blur(50px)";
+  circle.style.zIndex = "-1";
+
+  const animation = circle.animate(
+    [
+      { left: `${position.x}px`, top: `${position.y}px` },
+      { left: `${finalPosition.x}px`, top: `${finalPosition.y}px` },
+    ],
+    {
+      duration: duration * 1000,
+      iterations: 1,
+      easing: "linear",
+    }
+  );
+
+  animation.onfinish = () => {
+    circle.remove();
+    createRandomCircle();
+  };
+
+  rectangle.appendChild(circle);
+}
+
+function createRandomCircles() {
+  const numberOfCircles = Math.floor(Math.random() * 10) + 1;
+
+  for (let i = 0; i < numberOfCircles; i++) {
+    createRandomCircle();
+  }
+}
+
+createRandomCircles();
+
